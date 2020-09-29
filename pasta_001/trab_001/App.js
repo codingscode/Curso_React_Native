5df5103876d4e11c22b64138d7f17ad0d300cf7c
Heronnes Pereira <heronnestg@hotmail.com>
2020-09-29 00:00:24 -0300

    parte 1

--- pasta_001/trab_001/App.js
+++ pasta_001/trab_001/App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Testando React Native!</Text>
+      <Button title="Alterar Texto"/>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
+// análogo a CSS
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+})
