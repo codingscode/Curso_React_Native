bd4c20788f6ab34ee79ad9a16e3ea91bf50d0bc6
codinscode <heronnestg@hotmail.com>
2021-08-03 21:53:33 -0300

    parte 10

--- pasta_002/exemplo_2/App.js
+++ pasta_002/exemplo_2/App.js
@@ -4,8 +4,8 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={{ padding: 50, flexDirection: 'row', width: '80%', height: 300, justifyContent: 'space-around', alignItems: 'stretch' }}>
-      <View style={{ backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
+    <View style={{ padding: 50, flexDirection: 'row', width: '80%', height: 300, justifyContent: 'space-around', alignItems: 'center' }}>
+      <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
          <Text>1</Text>
       </View>
       <View style={{ backgroundColor: 'blue', justifyContent: 'center', alignItems: 'center' }}>
