54efc9f3cc5608f25293b2d64c3e0a384b5b0fb8
codingscode <heronnestg@hotmail.com>
2021-08-17 21:31:14 -0300

    parte 17

--- pasta_002/react_guide/App.js
+++ pasta_002/react_guide/App.js
@@ -1,6 +1,6 @@
 
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native'
 
 
 export default function App() {
@@ -24,12 +24,12 @@ export default function App() {
         <Button title="adicione" onPress={adicionargerenMeta} />
       </View>
       <Text>outro texto!</Text>
-      <View  >
+      <ScrollView>
          {metasCurso.map((cada, indice) => (
              <View style={estilos.itemLista} >
                 <Text key={indice} >{cada}</Text>
              </View>) )}
-      </View>
+      </ScrollView>
       
     </View>
   )
