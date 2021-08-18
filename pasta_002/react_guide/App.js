1ea554bb734625aae3410f0bc8340445ceacac40
codingscode <heronnestg@hotmail.com>
2021-08-17 21:32:25 -0300

    parte 18

--- pasta_002/react_guide/App.js
+++ pasta_002/react_guide/App.js
@@ -18,20 +18,23 @@ export default function App() {
 
 
   return (
-    <View style={estilos.tela} >
-      <View style={estilos.entradaContainer} >
-        <TextInput placeholder="Meta de curso" style={estilos.entrada} onChangeText={gerenMetaDigitada} value={metaDigitada} />
-        <Button title="adicione" onPress={adicionargerenMeta} />
+    <ScrollView>
+      <View style={estilos.tela} >
+        <View style={estilos.entradaContainer} >
+          <TextInput placeholder="Meta de curso" style={estilos.entrada} onChangeText={gerenMetaDigitada} value={metaDigitada} />
+          <Button title="adicione" onPress={adicionargerenMeta} />
+        </View>
+        <Text>outro texto!</Text>
+        <ScrollView>
+          {metasCurso.map((cada, indice) => (
+              <View style={estilos.itemLista} >
+                  <Text key={indice} >{cada}</Text>
+              </View>) )}
+        </ScrollView>
+        
       </View>
-      <Text>outro texto!</Text>
-      <ScrollView>
-         {metasCurso.map((cada, indice) => (
-             <View style={estilos.itemLista} >
-                <Text key={indice} >{cada}</Text>
-             </View>) )}
-      </ScrollView>
-      
-    </View>
+
+    </ScrollView>
   )
 }
 
